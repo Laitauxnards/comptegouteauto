687e89e0b12a30460de14dee2b884d385a4524ff
Léochan <81470534+l3ochan@users.noreply.github.com>
2023-05-09 17:37:53 +0200

    Update read_sensors.ino
    added % moist debug value

--- cases/analog/read_sensors.ino
+++ cases/analog/read_sensors.ino
@@ -1,15 +1,15 @@
   switch(state){
     case read_sensors:
       digitalWrite(br_start_water, LOW); //couper l'arrosage
-      night = analogRead(br_nuit); //a remplacer par un script photo résistance (avec quantum comme potentiomètre)
-      if (analogRead(br_lvl_eau) == LOW) { //s'assurer que le niveau d'eau est correct 
+      night = digitalRead(br_nuit); //a remplacer par un script photo résistance
+      if (digitalRead(br_lvl_eau) == LOW) { //s'assurer que le niveau d'eau est correct 
         Serial.println("Low water ! Switching state to low_water");
         state = low_water;
         break;
       }
-      if (analogRead(br_nuit)==LOW) {
+      if (digitalRead(br_nuit)==LOW) {
         Serial.println("Night is true");
-        if map(analogRead(0), 500, 250, 0, 100)<set_moist { //vérifier que la terre est moins humide que défini par l'utilisateur
+        if (map(analogRead(0), 520, 250, 0, 100)<set_moist) { //vérifier que la terre est moins humide que défini par l'utilisateur
           Serial.println("Soil is dry ! Switching state to check_rain");
           state = check_rain;
         } 
@@ -19,9 +19,11 @@
         }
       Serial.println("Case read_sensors successfully executed using following settings: ");
       Serial.print("br_nuit: ");
-      Serial.println(anallogRead(br_nuit));
+      Serial.println(digitalRead(br_nuit));
       Serial.print("br_lvl_eau: ");        
-      Serial.println(analogRead(br_lvl_eau));
+      Serial.println(digitalRead(br_lvl_eau));
+      Serial.print("br_soil_moist (%): ");        
+      Serial.println(map(analogRead(0), 520, 250, 0, 100));
       Serial.print("br_soil_moist: ");
       Serial.println(analogRead(br_soil_moist));
       Serial.println("_________");
