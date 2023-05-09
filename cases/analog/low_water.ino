4ecb91eec29b4061b905e765ce95256a80482397
MrRa3d <117953572+Ra3d0@users.noreply.github.com>
2023-05-09 15:06:48 +0200

    Update low_water.ino

--- cases/analog/low_water.ino
+++ cases/analog/low_water.ino
@@ -1,10 +1,10 @@
     case low_water:
       while (analogRead(br_lvl_eau) == HIGH) {
-        analogWrite(br_led_wat_low, HIGH);
+        digitalWrite(br_led_wat_low, HIGH);
       }
       Serial.println("Water level is good");  
       Serial.println("Case low_water successfully executed switching state to read_sensors");
       Serial.println("_________");  
-      analogWrite(br_led_wat_low, LOW);  
+      digitalWrite(br_led_wat_low, LOW);  
       state = read_sensors;
       break;
