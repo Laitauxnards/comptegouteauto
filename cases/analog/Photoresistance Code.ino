c44301834437a07539e970205c3c6e3fc09e0256
MrRa3d <117953572+Ra3d0@users.noreply.github.com>
2023-05-12 15:36:03 +0200

    Update Photoresistance Code.ino

--- cases/analog/Photoresistance Code.ino
+++ cases/analog/Photoresistance Code.ino
@@ -1,10 +1,12 @@
 int pr = A0;
 
 void setup() {
- pinMode(pr, INPUT);
+ pinMode(pr, INPUT_PULLUP);
+ Serial.begin(9600);
  } 
 
 void loop() {
     Serial.print("pr: ");
     Serial.println(analogRead(pr));
 }
+
