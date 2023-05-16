bcdbe7b450cca30176c5426a3604ecc1e4787327
Léochan <81470534+l3ochan@users.noreply.github.com>
2023-05-16 16:45:15 +0200

    Update Photoresistance Code.ino

--- cases/analog/Photoresistance Code.ino
+++ cases/analog/Photoresistance Code.ino
@@ -1,12 +1,19 @@
-int pr = A0;
+#define PHORORESISTOR_PIN A1
+#include <LiquidCrystal.h>
+LiquidCrystal lcd(8, 9, 4, 5, 6, 7);  
 
-void setup() {
- pinMode(pr, INPUT);
- Serial.begin(9600);
- } 
 
-void loop() {
-    Serial.print("lum: ");
-    Serial.println(analogRead(pr));
+void setup(){
+    lcd.begin(16, 2);
+    pinMode(A1, INPUT);
+    Serial.begin(9600);
 }
 
+void loop(){
+    lcd.setCursor(0, 0);  
+    lcd.print("lum: ");
+    lcd.print(analogRead(A1));
+    Serial.print("lum: ");
+    Serial.println(analogRead(A1));
+    delay(500);
+}
